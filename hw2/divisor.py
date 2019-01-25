21909aa631dcb593be969dbffb08951d87545472
Branden Kim <brandenk0@gmail.com>
2019-01-25 16:50:41 -0500

    HW2

---
+++ hw2/divisor.py
@@ -0,0 +1,15 @@
+# author: Branden Kim
+# assignment: 2
+# description: Prints out all of the divisors of a given number
+
+
+def get_divisor():
+    num = int(input('Enter an integer number: '))
+    return num, [str(i) for i in range(1, num // 2) if num % i == 0]
+
+
+if __name__ == '__main__':
+    num, divisors = get_divisor()
+
+    print(f'Divisors of {num}:')
+    print('\n'.join(divisors))
---
+++ hw2/greetings_game.py
@@ -0,0 +1,31 @@
+# author: Branden Kim
+# assignment: 2
+# description: Get a random greeting for a user
+
+import random
+
+
+def get_greeting(name, low=0, high=24):
+    rand_num = random.randint(low, high)
+
+    if rand_num >= 6 and rand_num <= 12:
+        return f'Good morning, {name}!'
+    elif rand_num > 12 and rand_num <= 15:
+        return f'Good afternoon, {name}!'
+    elif rand_num > 15 and rand_num <= 20:
+        return f'Good evening, {name}!'
+    else:
+        return f'Good night, {name}!'
+
+
+def greetings_game():
+    name = input('What is your name? ')
+    num_greetings = int(input('How many greetings do you want to see? '))
+
+    for _ in range(num_greetings):
+        print(get_greeting(name))
+
+
+if __name__ == '__main__':
+    greetings_game()
+    
